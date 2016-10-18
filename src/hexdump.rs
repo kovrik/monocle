5896ec78a6e5395b9c7e1a9a962a93814d470670
kovrik <kovrik0@gmail.com>
2016-10-18 21:03:29 +1300

    Remove old print_hexdump

--- src/hexdump.rs
+++ src/hexdump.rs
@@ -1,28 +1,9 @@
+use std::str;
+
 // FIXME byte order
-
-// pub fn print_hexdump(data: &[u8]) {
-//     let mut c = 0;
-//     let mut word: u16 = 0;
-//     for b in data {
-//         if (c % 16) == 0 {
-//             println!("");
-//             print!("{:08x}: ", c);
-//         }
-//         c = c + 1;
-//         if (c % 2) == 0 {
-//             print!("{:04x} ", word + (*b as u16));
-//         } else {
-//             word = (*b as u16) << 8;
-//         }
-//     }
-//     if (&data.len() % 2) != 0 {
-//         print!("{:04x} ", word);
-//     }
-// }
-
 pub fn print_hexdump(data: &[u8]) {
     let mut c = 0;
-    let mut line: [u16; 16] = [0; 16];
+    let mut line: [u8; 16] = [0; 16];
     for b in data {
         if (c % 16) == 0 {
             line = [0; 16];
@@ -30,20 +11,28 @@ pub fn print_hexdump(data: &[u8]) {
             print!("{:08x}: ", c);
         }
         
-        line[c % 16] = *b as u16;
+        line[c % 16] = *b;
         c = c + 1;
         if (c % 16) == 0 {
             // print line
             for b in 0..8 {
-                let w = ((line[2*b] as u16) << 8) + line[2*b + 1];
+                let w = ((line[2*b] as u16) << 8) + (line[2*b + 1] as u16);
                 print!(" {:04x}", w);
             }
+            // TODO
+            // let utf8_replacement_bytes: [u8; 3] = [0xEF, 0xBF, 0xBD];
+            // let utf8_replacement = String::from_utf8_lossy(&utf8_replacement_bytes);
+            // let s = String::from_utf8_lossy(&line);
+            // let s = s.replace(utf8_replacement, ".");
+            // print!("  {}", s);
         }
     }
     if (c % 16) != 0 {
-        for b in 0..8 {
-            let w = ((line[2*b] as u16) << 8) + line[2*b + 1];
+        for b in 0..((c % 16) / 2) {
+            let w = ((line[2*b] as u16) << 8) + (line[2*b + 1] as u16);
             print!(" {:04x}", w);
         }
+        // let s = String::from_utf8_lossy(&line);
+        // print!("  {}", s);
     }
 }
